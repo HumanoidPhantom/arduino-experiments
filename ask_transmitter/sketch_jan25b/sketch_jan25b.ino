ea2923b4a6591df47ce598a6a7f8a6453bf67183
Humanoid_phantom <humanoidphantom@gmail.com>
2018-01-26 21:22:43 +0300

    add message input

--- ask_transmitter/sketch_jan25b/sketch_jan25b.ino
+++ ask_transmitter/sketch_jan25b/sketch_jan25b.ino
@@ -1,46 +1,117 @@
 #include <RH_ASK.h>
-#include <SPI.h> // Not actualy used but needed to compile
-#include <rfscomm.h>
-
-// I/O defines
-#define P_TX         2 // Tx digital pin
-
-// RF defines
-#define MY_ID     0xA5 // Decimal value: 165 - One Byte, range (0-255) | (0x00-0xFF)
-#define DEVICE_ID 0x1B // Decimal value:  27 - One Byte, range (0-255) | (0x00-0xFF)
-#define DATA      0x12 // Decimal value:  18 - One Byte, range (0-255) | (0x00-0xFF)
-
-// RF Object
-RFSCOMM rf(MY_ID);
+#include <SPI.h> // Not actually used but needed to compile
 
 RH_ASK driver(2000, 9, 2, 10); // ESP8266 or ESP32: do not use pin 11
 
+const int firstButtonPin = 4;     // the number of the pushbutton pin
+const int secondButtonPin = 5;
+
+//const byte msgLen = 200;
+//char msg[msgLen];
+
+//const int ledPin =  13;
+int firstButtonPushCounter = 0;   // counter for the number of button presses
+int firstButtonState = 0;         // current state of the button
+int lastFirstButtonState = 0;     // previous state of the button
+
+int secondButtonPushCounter = 0;   // counter for the number of button presses
+int secondButtonState = 0;         // current state of the button
+int lastSecondButtonState = 0;     // previous state of the button
+
+boolean showHelpMessage = true;
+
 void setup()
 {
-    Serial.begin(9600);  // Debugging only
+    pinMode(firstButtonPin, INPUT);
+    pinMode(secondButtonPin, INPUT);
+    
+    Serial.begin(9600);    // Debugging only
     if (!driver.init())
          Serial.println("init failed");
-
-    rf.config(P_TX, NO_USE, MAN_1200);
-    
-    Serial.print("\nSystem with ID 0x"); Serial.print(MY_ID, HEX); Serial.print(", configured to transmit\n");
-    Serial.print("\n-------------------------------------------------------\n");
 }
 
 void loop()
-{
-    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-    uint8_t buflen = sizeof(buf);
+{  
+    helpMessage();
+    recvWithEndMarker();
+    buttonsHandler();
+}
 
-    rf.send(DEVICE_ID, DATA);
-    Serial.print("\nPackage sent:\n");
-    Serial.print("[My ID: 0x"); Serial.print(MY_ID, HEX); Serial.print(" | ");
-    Serial.print("To device with ID: 0x"); Serial.print(DEVICE_ID, HEX); Serial.print(" | ");
-    Serial.print("Data: 0x"); Serial.print(DATA, HEX); Serial.print("\n");
-    Serial.print("\n-------------------------------------------------------\n");
-    delay(1000);
+void helpMessage() {
+    if (showHelpMessage) {
+        Serial.println("Enter your message: ");
+        showHelpMessage = false;  
+    }
+  }
 
-//    driver.send((uint8_t *)msg, strlen(msg));
-//    driver.waitPacketSent();
-//    Serial.println("done");
+void recvWithEndMarker() {
+    if (Serial.available() > 0) {
+        String strMsg = Serial.readString();
+        int msgLen = strMsg.length();
+        char msg[msgLen];
+        int i;
+        for (i = 0; i < msgLen; i++) {
+               msg[i] = strMsg[i];
+          }
+        msg[i] = '\0';
+        showNewData(msg);
+  }
+}
+
+void showNewData(char *msg) {
+      sendMessage(msg);
+      Serial.print("You sent message: ");
+      Serial.println(msg);
+      showHelpMessage = true;
+  }
+
+void sendMessage(char *msg) {
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+  }
+
+void encryptMsg(char *msg) {
+      char *encKey = "this is my key";
+      
+  }
+
+void buttonsHandler() {
+  firstButtonState = digitalRead(firstButtonPin);
+    secondButtonState = digitalRead(secondButtonPin);
+
+    if (firstButtonState != lastFirstButtonState) {
+      // if the state has changed, increment the counter
+      if (firstButtonState == HIGH) {
+        // if the current state is HIGH then the button went from off to on:
+        firstButtonPushCounter++;
+        Serial.println("on");
+        Serial.print("number of button pushes: ");
+        Serial.println(firstButtonPushCounter);
+      } else {
+        // if the current state is LOW then the button went from on to off:
+        Serial.println("off");
+      }
+      // Delay a little bit to avoid bouncing
+      delay(50);
+    }
+    // save the current state as the last state, for next time through the loop
+    lastFirstButtonState = firstButtonState;
+
+    if (secondButtonState != lastSecondButtonState) {
+      // if the state has changed, increment the counter
+      if (secondButtonState == HIGH) {
+        // if the current state is HIGH then the button went from off to on:
+        secondButtonPushCounter++;
+        Serial.println("second button on");
+        Serial.print("number of second button pushes: ");
+        Serial.println(secondButtonPushCounter);
+      } else {
+        // if the current state is LOW then the button went from on to off:
+        Serial.println("second buttton off");
+      }
+      // Delay a little bit to avoid bouncing
+      delay(50);
+    }
+    // save the current state as the last state, for next time through the loop
+    lastSecondButtonState = secondButtonState;
 }
