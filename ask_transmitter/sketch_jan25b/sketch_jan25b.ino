ff8d6a20f4bdb6297f79622d6ab5f6514e1dc36c
Humanoid_phantom <humanoidphantom@gmail.com>
2018-01-27 15:04:20 +0300

    encryption turned on with hardcoded messages and key

--- ask_transmitter/sketch_jan25b/sketch_jan25b.ino
+++ ask_transmitter/sketch_jan25b/sketch_jan25b.ino
@@ -1,3 +1,19 @@
+#include <blockcipher_descriptor.h>
+#include <debug.h>
+#include <hashfunction_descriptor.h>
+#include <keysize_descriptor.h>
+#include <sha3-api.h>
+#include <stack_measuring.h>
+#include <streamcipher_descriptor.h>
+#include <string-extras.h>
+#include <AES.h>
+#include <CBC.h>
+#include <Crypto.h>
+#include <string.h>
+#include <SHA256.h>
+
+#define HASH_SIZE 32
+
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 
@@ -6,8 +22,8 @@ RH_ASK driver(2000, 9, 2, 10); // ESP8266 or ESP32: do not use pin 11
 const int firstButtonPin = 4;     // the number of the pushbutton pin
 const int secondButtonPin = 5;
 
-//const byte msgLen = 200;
-//char msg[msgLen];
+#define MAX_PLAINTEXT_SIZE 64
+#define MAX_CIHPERTEXT_SIZE 64
 
 //const int ledPin =  13;
 int firstButtonPushCounter = 0;   // counter for the number of button presses
@@ -19,11 +35,17 @@ int secondButtonState = 0;         // current state of the button
 int lastSecondButtonState = 0;     // previous state of the button
 
 boolean showHelpMessage = true;
+byte cbc_iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+
+SHA256 sha256;
+CBC<AES256> cbcaes256;
+char *aes_key = (char *)"this is test";
 
 void setup()
 {
     pinMode(firstButtonPin, INPUT);
     pinMode(secondButtonPin, INPUT);
+
     
     Serial.begin(9600);    // Debugging only
     if (!driver.init())
@@ -34,7 +56,7 @@ void loop()
 {  
     helpMessage();
     recvWithEndMarker();
-    buttonsHandler();
+//    buttonsHandler();
 }
 
 void helpMessage() {
@@ -45,34 +67,90 @@ void helpMessage() {
   }
 
 void recvWithEndMarker() {
-    if (Serial.available() > 0) {
-        String strMsg = Serial.readString();
-        int msgLen = strMsg.length();
-        char msg[msgLen];
-        int i;
-        for (i = 0; i < msgLen; i++) {
-               msg[i] = strMsg[i];
-          }
-        msg[i] = '\0';
-        showNewData(msg);
-  }
+//    if (Serial.available() > 0) {
+//        String strMsg = Serial.readString();
+//        int msgLen = strMsg.length();
+//        char msg[msgLen];
+//        int i;
+//        for (i = 0; i < msgLen; i++) {
+//               msg[i] = strMsg[i];
+//          }
+//        msg[i] = '\0';
+//        showNewData(msg);
+//  }
+  char *msg = (char *) "Nice to meet you";
+  showNewData(msg);
+  delay(1000);
 }
 
 void showNewData(char *msg) {
-      sendMessage(msg);
-      Serial.print("You sent message: ");
-      Serial.println(msg);
-      showHelpMessage = true;
+     Serial.print("You sent message: ");
+     Serial.println(msg);
+     encryptMsg(msg, strlen(msg));
+     showHelpMessage = true;
   }
 
-void sendMessage(char *msg) {
-    driver.send((uint8_t *)msg, strlen(msg));
+void sendMessage(char *msg, size_t msg_len) {
+    Serial.println(msg_len);
+    driver.send((uint8_t *)msg, msg_len);
     driver.waitPacketSent();
   }
 
-void encryptMsg(char *msg) {
-      char *encKey = "this is my key";
-      
+void encryptMsg(const char *msg, size_t msg_len) {
+      Hash *hash = &sha256;
+      const char *key = aes_key;
+      size_t k_size = strlen(aes_key);
+      size_t posn;
+      uint8_t hashKey[HASH_SIZE];
+      hash->reset();
+      posn = 0;
+      hash->update(key + posn, k_size);
+      hash->finalize(hashKey, sizeof(hashKey));
+
+      encCBCMode(msg, msg_len, hashKey, sizeof(hashKey));
+  }
+
+void encCBCMode(const char *msg, size_t msg_len, byte *hashKey, size_t hashKeySize) {
+    Cipher *cipher = &cbcaes256;    
+    byte *myhashKey = hashKey;
+    size_t m_real_size = msg_len;
+    size_t append_block = 0;
+    if (m_real_size % 16 == 0) {
+        append_block = 16;
+      } else {
+        append_block = 16 - m_real_size % 16;  
+      }
+    size_t m_size = m_real_size + append_block;
+    uint8_t cur_msg[m_size];
+    byte output[m_size];
+    for (int i = 0; i < m_size; i++) {
+        if (i < m_real_size) {
+            cur_msg[i] = msg[i];
+          } else {
+            if (append_block == 16) {
+              cur_msg[i] = 0;  
+            } else {
+              cur_msg[i] = append_block;
+            }
+          }
+      }
+
+    cipher->clear();
+
+    if (!cipher->setKey(myhashKey, cipher->keySize())) {
+        Serial.print("setKey ");
+        return;
+      }
+
+    if (!cipher->setIV(cbc_iv, cipher->ivSize())) {
+      Serial.print("setIV ");
+      return;
+    }
+
+    memset(output, 0xBA, sizeof(output));
+
+    cipher->encrypt(output+0, cur_msg+0, m_size);
+    sendMessage((char *) output, sizeof(output));
   }
 
 void buttonsHandler() {
